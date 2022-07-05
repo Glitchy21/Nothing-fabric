9426ed4fa139242449be0723a51dede9081178f8
Glitchy21 <creeperchuu2121@gmail.com>
2022-07-05 15:15:37 -0400

    Initial commit

---
+++ src/main/java/net/glitchy/nothing/Nothing.java
@@ -0,0 +1,14 @@
+package net.glitchy.nothing;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Nothing implements ModInitializer {
+	public static final String MOD_ID = "nothing";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/glitchy/nothing/NothingClient.java
@@ -0,0 +1,10 @@
+package net.glitchy.nothing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NothingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
